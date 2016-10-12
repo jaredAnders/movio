5c6f01180357b2f92e6695346fc58227ea30e6da
Jared Gilbert <jaredandersgilbert@gmail.com>
2016-10-12 08:12:51 -0400

    allows creation of new channels

--- app/controllers/contributor/channels_controller.rb
+++ app/controllers/contributor/channels_controller.rb
@@ -4,4 +4,19 @@ class Contributor::ChannelsController < ApplicationController
   def new
     @channel = Channel.new
   end
+
+  def create
+    @channel = current_user.channels.create(channel_params)
+    redirect_to contributor_channel_path(@channel)
+  end
+
+  def show
+    @channel = Channel.find(params[:id])
+  end
+
+  private
+
+  def channel_params
+    params.require(:channel).permit(:title, :description, :cost)
+  end
 end
