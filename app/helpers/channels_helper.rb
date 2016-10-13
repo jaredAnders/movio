0f8cc5e178a2d5f3260f8b1b15f3be555b85bcf7
Jared Gilbert <jaredandersgilbert@gmail.com>
2016-10-12 20:40:31 -0400

    adds text color classes for flat buttons

--- app/helpers/channels_helper.rb
+++ app/helpers/channels_helper.rb
@@ -1,7 +1,7 @@
 module ChannelsHelper
   def admin_button
     if current_user && current_user == @channel.user
-      link_to 'Admin', contributor_channel_path(@channel), class: 'btn-flat waves-light waves-effect right'
+      link_to 'Admin', contributor_channel_path(@channel), class: 'btn-flat waves-light waves-effect right red-text'
     end
   end
 end
