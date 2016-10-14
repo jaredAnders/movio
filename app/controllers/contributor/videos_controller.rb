70624ea6fb2e2b6682a23bd307ab654d1cfbd476
Jared Gilbert <jaredandersgilbert@gmail.com>
2016-10-14 08:05:27 -0400

    restricts access to video creation, refactors contributor video controller to reduce db queries

--- app/controllers/contributor/videos_controller.rb
+++ app/controllers/contributor/videos_controller.rb
@@ -1,16 +1,28 @@
 class Contributor::VideosController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_auth
+
   def new
-    @playlist = Playlist.find(params[:playlist_id])
     @video = Video.new
   end
 
   def create
-    @playlist = Playlist.find(params[:playlist_id])
-    @video = @playlist.videos.create(video_params)
-    redirect_to contributor_channel_path(@playlist.channel)
+    @video = current_playlist.videos.create(video_params)
+    redirect_to contributor_channel_path(current_playlist.channel)
   end
 
   private
+
+  def require_auth
+    if current_playlist.channel.user != current_user
+      return render text: 'Unauthorized', status: :unauthorized
+    end
+  end
+
+  helper_method :current_playlist
+  def current_playlist
+    @playlist ||= Playlist.find(params[:playlist_id])
+  end
 
   def video_params
     params.require(:video).permit(:title, :description)
