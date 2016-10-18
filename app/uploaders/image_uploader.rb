b2884acd6c65d4e5f2a1b3a0a13f521577ca85cd
Jared Gilbert <jaredandersgilbert@gmail.com>
2016-10-18 11:14:04 -0400

    adds and configures minimagick for image uploads

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,7 +2,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
@@ -23,6 +23,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
+  process resize_to_fill: [800, 350]
   # process scale: [200, 300]
   #
   # def scale(width, height)
