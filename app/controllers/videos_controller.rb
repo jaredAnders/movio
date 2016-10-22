3ec66d3e3723a489f743b85da67edec5d3f09a49
Jared Gilbert <jaredandersgilbert@gmail.com>
2016-10-22 11:50:30 -0400

    restricts video access to subscribers only, displays alert on channel page if not subscribed

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,13 +1,26 @@
 class VideosController < ApplicationController
   before_action :authenticate_user!
+  before_action :require_subscription
 
   def show
   end
 
   private
 
+  def require_subscription
+    if !current_user.subscribed?(current_channel)
+      redirect_to channel_path(current_channel), alert: 'Please subscribe to view videos'
+    end
+  end
+
   helper_method :current_video
   def current_video
     @current_video ||= Video.find(params[:id])
   end
+
+  def current_channel
+    @current_channel ||= current_video.playlist.channel
+  end
+
+
 end
