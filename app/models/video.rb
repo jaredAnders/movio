70bdc7807d5c67c13027270255ee455bafed0cdf
Jared Gilbert <jaredandersgilbert@gmail.com>
2016-10-24 16:22:31 -0400

    allows ranking of videos by position within playlists, displays in order of position in channel detail page

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,6 +2,9 @@ class Video < ActiveRecord::Base
   belongs_to :playlist
   mount_uploader :video, VideoUploader
 
+  include RankedModel
+  ranks :row_order, :with_same => :playlist_id
+
   validates :title, presence: true
   validates :description, presence: true
 end
